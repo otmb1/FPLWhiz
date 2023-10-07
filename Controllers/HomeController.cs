1eb96a5d0d7aab4478da86c3af5315ae2140a8ec
Ore Deru <93195185+otmb1@users.noreply.github.com>
2023-10-07 22:28:14 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FPLWhiz.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FPLWhiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult GetRecommendation(string playerName)
+        {
+            try
+            {
+                var fantasyRecommendation = new FantasyRecommendation();
+                string recommendation = fantasyRecommendation.GetRecommendation(playerName);
+                return View("Recommendation", recommendation);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while generating a recommendation.");
+                throw; // Rethrow the exception for further handling
+            }
+        }
+    }
+}
---
+++ Pages/Recommendation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FPLWhiz.Pages
+{
+    public class RecommendationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
