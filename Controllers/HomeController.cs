f514c9558da006304798c7b2f31dce3db4081370
Ore Deru <93195185+otmb1@users.noreply.github.com>
2023-10-08 22:21:16 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using FPLWhiz.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    private readonly FantasyRecommendation _fantasyRecommendation;
+
+    public HomeController(FantasyRecommendation fantasyRecommendation)
+    {
+        _fantasyRecommendation = fantasyRecommendation;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult GetRecommendation(string playerName)
+    {
+        try
+        {
+            string recommendation = _fantasyRecommendation.GetRecommendation(playerName);
+            return PartialView("_RecommendationPartial", recommendation);
+        }
+        catch (Exception ex)
+        {
+            return PartialView("_RecommendationPartial", "An error occurred.");
+        }
+    }
+}
---
+++ Models/FantasyRecommendation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FPLWhiz.Models
+{
+    public class FantasyRecommendation
+    {
+        public string GetRecommendation(string playerName)
+        {
+            if (playerName.Contains("Saka"))
+            {
+                return $"Recommendation for {playerName}: Buy!";
+            }
+            else
+            {
+                return $"Recommendation for {playerName}: Hold or Sell.";
+            }
+        }
+    }
+}
