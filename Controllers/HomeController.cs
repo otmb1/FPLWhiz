45ddb6f2432e8c6e6305c3d6ff429c2d0e09abe5
Ore Deru <93195185+otmb1@users.noreply.github.com>
2023-10-10 21:46:37 +0100

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,28 @@
 ﻿using FPLWhiz.Models;
 using Microsoft.AspNetCore.Mvc;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 public class HomeController : Controller
 {
     private readonly FantasyRecommendation _fantasyRecommendation;
+    private readonly List<Player> _players;
 
     public HomeController(FantasyRecommendation fantasyRecommendation)
     {
         _fantasyRecommendation = fantasyRecommendation;
+
+        var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+        using (var reader = new StreamReader("FPLData.csv"))
+        using (var csv = new CsvReader(reader, csvConfig))
+        {
+            _players = csv.GetRecords<Player>().ToList();
+        }
     }
 
     public IActionResult Index()
@@ -27,5 +42,36 @@ public class HomeController : Controller
         {
             return PartialView("_RecommendationPartial", "An error occurred.");
         }
+    }
+
+    [HttpGet]
+    public IActionResult ComparePlayers()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult ComparePlayers(string player1Name, string player2Name)
+    {
+        var player1 = _players.FirstOrDefault(p => p.second_name.Contains(player1Name, StringComparison.OrdinalIgnoreCase));
+        var player2 = _players.FirstOrDefault(p => p.second_name.Contains(player2Name, StringComparison.OrdinalIgnoreCase));
+
+        if (player1 == null || player2 == null)
+        {
+            return View("ComparePlayers", new CompareViewModel
+            {
+                Player1 = player1,
+                Player2 = player2,
+                ErrorMessage = "One or both players not found."
+            });
+        }
+
+        var viewModel = new CompareViewModel
+        {
+            Player1 = player1,
+            Player2 = player2
+        };
+
+        return View("ComparePlayers", viewModel);
     }
 }
