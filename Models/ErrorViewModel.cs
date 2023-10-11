6035b47946a7210f776cb67425aa84263bc25cf5
Ore Deru <93195185+otmb1@users.noreply.github.com>
2023-10-11 19:57:32 +0100

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FPLWhiz.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
